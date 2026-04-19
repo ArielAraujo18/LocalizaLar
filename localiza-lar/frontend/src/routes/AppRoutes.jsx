9ca90b7cff0e7a63369e539606e8dce7b57b6ebe
ArielAraujo18 <arielaraujo120@gmail.com>
2026-04-18 23:23:08 -0300

    Update

--- localiza-lar/frontend/src/routes/AppRoutes.jsx
+++ localiza-lar/frontend/src/routes/AppRoutes.jsx
@@ -1,21 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Cadastro from "../pages/Cadastro/cadastro";
 import Login from "../pages/Login/login";
-import Home from "../pages/Home/home"
-import Sidebar from "../components/Sidebar/Sidebar";
+import Home from "../pages/Home/home";
 
-function AppRoutes(){
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/cadastrar" element={<Cadastro />}/>
-                <Route path="/home" element={<Home />}/>
-                <Route path="/sidebar" element={<Sidebar />}/>
-            </Routes>
-        </BrowserRouter>
-    )
+import AppSidebar from "../components/Sidebar/AppSidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <SidebarProvider>
+        <AppSidebar />
+
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastrar" element={<Cadastro />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+
+      </SidebarProvider>
+    </BrowserRouter>
+  );
 }
 
 export default AppRoutes;
