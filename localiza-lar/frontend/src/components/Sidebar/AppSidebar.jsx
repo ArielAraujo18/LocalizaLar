f572a9de1882dfdf2e8aed8aeae9ad1bb0409365
ArielAraujo18 <arielaraujo120@gmail.com>
2026-04-25 04:02:10 -0300

    Update

--- localiza-lar/frontend/src/components/Sidebar/AppSidebar.jsx
+++ localiza-lar/frontend/src/components/Sidebar/AppSidebar.jsx
@@ -5,24 +5,41 @@ import {
   SidebarGroup,
   SidebarHeader,
 } from "@/components/ui/sidebar";
+import { Home, Building2, Users, User } from "lucide-react";
 
 export default function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader>
-        <div className="p-4 font-bold">
-          <img src="" alt="" />
-          <span className="font-bold">LocalizaLar</span>
+        <div className="p-4 flex items-center gap-2">
+          <img src="/Pin.png"
+          alt="logo"
+          className="w-8 h-8 object-contain" />
+          <div className="flex flex-col leading-tight">
+            <span className="font-bold ">LocalizaLar</span>
+            <span className="text-xs text-gray-500">Encontre o seu lar</span>
           </div>
+        </div>
+          <hr className="w-full" />
       </SidebarHeader>
 
       <SidebarContent>
         <SidebarGroup>
-          <button className="w-full text-left p-2 hover:bg-muted">
-            Home
+          <button className="w-full flex gap-2 items-center text-left p-2 text-pink-600 hover:bg-muted ">
+            <Home size={18} />
+            <span>Início</span>
           </button>
-          <button className="w-full text-left p-2 hover:bg-muted">
-            Perfil
+          <button className="w-full flex gap-2 items-center text-left p-2 text-pink-600 hover:bg-muted ">
+            <Building2 size={18} />
+            <span>Imóveis</span>
+          </button>
+          <button className="w-full flex gap-2 items-center text-left p-2 text-pink-600 hover:bg-muted ">
+            <Users size={18} />
+            <span>Encontrar coleagas</span>
+          </button>
+          <button className="w-full flex gap-2 items-center text-left p-2 text-pink-600 hover:bg-muted ">
+            <User size={18} />
+            <span>Meu perfil</span>
           </button>
         </SidebarGroup>
       </SidebarContent>
