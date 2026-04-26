2c64081a3366adac9d23525e04745f783a84e7e9
ArielAraujo18 <arielaraujo120@gmail.com>
2026-04-26 13:48:16 -0300

    Tem que alterar o layout pra mobile

--- localiza-lar/frontend/src/components/Sidebar/AppSidebar.jsx
+++ localiza-lar/frontend/src/components/Sidebar/AppSidebar.jsx
@@ -5,11 +5,18 @@ import {
   SidebarGroup,
   SidebarHeader,
 } from "@/components/ui/sidebar";
-import { Home, Building2, Users, User } from "lucide-react";
+import { Home, Building2, Users, User, LogOut} from "lucide-react";
+import { useState } from "react";
 
 export default function AppSidebar() {
+  
+  const [user, setUser] = useState(() => {
+    const data = localStorage.getItem("user");
+    return data ? JSON.parse(data) : null;
+  });
+
   return (
-    <Sidebar>
+    <Sidebar className="w-64 hidden md:flex flex-col h-screen">
       <SidebarHeader>
         <div className="p-4 flex items-center gap-2">
           <img src="/Pin.png"
@@ -23,21 +30,21 @@ export default function AppSidebar() {
           <hr className="w-full" />
       </SidebarHeader>
 
-      <SidebarContent>
+      <SidebarContent className="flex-1 overflow-y-auto">
         <SidebarGroup>
-          <button className="w-full flex gap-2 items-center text-left p-2 text-pink-600 hover:bg-muted ">
+          <button className="w-full flex gap-2 items-center text-left p-2 text-pink-600 hover:text-pink-300">
             <Home size={18} />
             <span>Início</span>
           </button>
-          <button className="w-full flex gap-2 items-center text-left p-2 text-pink-600 hover:bg-muted ">
+          <button className="w-full flex gap-2 items-center text-left p-2 text-pink-600 hover:text-pink-300">
             <Building2 size={18} />
             <span>Imóveis</span>
           </button>
-          <button className="w-full flex gap-2 items-center text-left p-2 text-pink-600 hover:bg-muted ">
+          <button className="w-full flex gap-2 items-center text-left p-2 text-pink-600 hover:text-pink-300">
             <Users size={18} />
             <span>Encontrar coleagas</span>
           </button>
-          <button className="w-full flex gap-2 items-center text-left p-2 text-pink-600 hover:bg-muted ">
+          <button className="w-full flex gap-2 items-center text-left p-2 text-pink-600 hover:text-pink-300">
             <User size={18} />
             <span>Meu perfil</span>
           </button>
@@ -45,7 +52,17 @@ export default function AppSidebar() {
       </SidebarContent>
 
       <SidebarFooter>
-        <button className="p-2 w-full text-left">Sair</button>
+        <div className="flex flex-col m-1 bg-pink-100 p-2 rounded-[10px]">
+          <span>{user.name}</span>
+          <span>{user.email}</span>
+        </div>
+        <hr />
+          <div className="p-6 flex items-center justify-center text-red-900 hover:text-red-400 ">
+            <button className="flex items-center gap-1">
+                <LogOut size={18} />
+                <span>Sair</span>
+            </button>
+          </div>
       </SidebarFooter>
     </Sidebar>
   );
--- localiza-lar/frontend/src/pages/Home/home.jsx
+++ localiza-lar/frontend/src/pages/Home/home.jsx
@@ -7,14 +7,19 @@ function Home() {
   });
 
   return (
-    <div>
-      {user && (
-        <>
-          <img src={user.picture} width="80" />
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
-        </>
-      )}
+    <div className="ml-64 flex justify-center m-6">
+      <div className="w-full min-w-[60rem] ">
+
+        <div className="flex flex-col m-4">
+          <span className="font-bold text-pink-600 text-5xl family-sans">Meu perfil</span>
+          <span className="font-light text-pink-600">Preencha suas informações para conectar-se com outros estudantes</span>
+        </div>
+
+        <section className="bg-white mt-11 p-4 rounded-[15px] h-[17rem]">
+          <span>Foto de perfil</span>
+        </section>
+
+      </div>
     </div>
   );
 }
